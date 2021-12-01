4bbebe2ff648031ef1177563ba9aa56a7c43aa73
Cam Schwartz <cameronschwartz@oakland.edu>
2021-12-01 17:47:45 -0500

    inital commit

--- index.js
+++ index.js
@@ -3,55 +3,25 @@ const path = require("path");
 const fs = require("fs");
 const { create } = require("domain");
 
+
 const server = http.createServer((req, res) => {
     if(req.method == 'GET' && req.url == '/')
-    {
+        {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'text/html');
         fs.createReadStream('./index.html').pipe(res);
-    }
-    else if(req.method == 'GET' && req.url == '/style_sheet.css')
-    {
+        }
+    else if(req.method == 'GET' && req.url == '/styles/style_sheet.css')
+        {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'text/css');
-        fs.createReadStream('./style_sheet.css').pipe(res);
-    }
-    else if(req.method == "GET" && req.url == '/home')
-    {
-        res.statusCode == 200;
-        res.setHeader('Content-Type', 'application/json');
-    }
-    else if(req.method == "POST" && req.url == "/insert")
-    {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain');
+        fs.createReadStream('./styles/style_sheet.css').pipe(res);
+        }
 
-        var content = '';
-        req.on('data', function(data){
-            content += data;
+  let extName = path.extname(filePath);
 
-            var obj = JSON.parse(content);
+  let contentType = "text/html";
 
-            console.log("The UserName is: "+ obj.name);
-            console.log("The comment is: "+ obj.message);
-            var conn = con.getConnection();
-
-            conn.query('INSERT INTO comments.comments (comments.userName, comments.comment) VALUES (?,?)',[obj.name,obj.message], function(error, results, fields){
-            if(error) throw error;
-            console.log("Success!");
-        });
-
-        conn.end();
-        res.end("Success!");
-        });
-    }
-    else if(req.method == "GET" && req.url == '/comment_box.js')
-    {
-        res.writeHead(200, {"Content-Type":"text/javascript"});
-        fs.createReadStream("./comment_box.js").pipe(res);
-    }
-});
-  
   switch (extName) {
     case ".js":
       contentType = "text/javascript";
@@ -83,17 +53,15 @@ const server = http.createServer((req, res) => {
           }
         );
       } else {
-        
         res.writeHead(500);
         res.end(`Server Error ${err.code}`);
       }
     } else {
-
       res.writeHead(200, { "Content-Type": contentType });
       res.end(content, "utf8");
     }
+  });
 });
 
 const PORT = process.env.PORT || 3000;
-
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
