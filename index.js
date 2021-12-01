cfc56af1e2557bc08c1ce21f097ffaa03ccabdad
Cam Schwartz <cameronschwartz@oakland.edu>
2021-12-01 17:53:58 -0500

    inital commit

--- index.js
+++ index.js
@@ -3,20 +3,27 @@ const path = require("path");
 const fs = require("fs");
 const { create } = require("domain");
 
-
 const server = http.createServer((req, res) => {
-    if(req.method == 'GET' && req.url == '/')
-        {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/html');
-        fs.createReadStream('./index.html').pipe(res);
-        }
-    else if(req.method == 'GET' && req.url == '/styles/style_sheet.css')
-        {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/css');
-        fs.createReadStream('./styles/style_sheet.css').pipe(res);
-        }
+  let filePath = path.join(
+    __dirname,
+    "public",
+    req.url === "/" ? "index.html" : req.url
+  );
+  let filePath = path.join(
+    __dirname,
+    "public",
+    req.url === "/" ? "blog.html" : req.url
+  );
+  let filePath = path.join(
+    __dirname,
+    "public",
+    req.url === "/" ? "my_fun_facts.html" : req.url
+  );
+  let filePath = path.join(
+    __dirname,
+    "public",
+    req.url === "/" ? "my_resume.html" : req.url
+  );
 
   let extName = path.extname(filePath);
 
@@ -40,11 +47,9 @@ const server = http.createServer((req, res) => {
       break;
   }
 
-  
   fs.readFile(filePath, (err, content) => {
     if (err) {
       if (err.code == "ENOENT") {
-        
         fs.readFile(
           path.join(__dirname, "public", "404.html"),
           (err, content) => {
@@ -63,5 +68,7 @@ const server = http.createServer((req, res) => {
   });
 });
 
+
 const PORT = process.env.PORT || 3000;
+
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
