4a1f11e47750b28fe532fbcc6272119f2ce164ba
Cam Schwartz <cameronschwartz@oakland.edu>
2021-12-03 17:57:46 -0500

    inital commit

--- index.js
+++ index.js
@@ -1,61 +1,29 @@
-const http = require("http");
+const express = require("express");
 const path = require("path");
-const fs = require("fs");
-const { create } = require("domain");
+const ejs = require("ejs");
+const app = express();
 
-const server = http.createServer((req, res) => {
-  let filePath = path.join(
-    __dirname,
-    "public",
-    req.url === "/" ? "index.html" : req.url
-  );
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
 
-  let extName = path.extname(filePath);
-
-  let contentType = "text/html";
-
-  console.log(path.join(__dirname, 'html', 'blog.html'));
-
-  switch (extName) {
-    case ".js":
-      contentType = "text/javascript";
-      break;
-    case ".css":
-      contentType = "text/css";
-      break;
-    case ".json":
-      contentType = "application/json";
-      break;
-    case ".png":
-      contentType = "image/png";
-      break;
-    case ".jpg":
-      contentType = "image/jpg";
-      break;
-  }
-
-  fs.readFile(filePath, (err, content) => {
-    if (err) {
-      if (err.code == "ENOENT") {
-        fs.readFile(
-          path.join(__dirname, "public", "404.html"),
-          (err, content) => {
-            res.writeHead(200, { "Content-Type": "text/html" });
-            res.end(content, "utf8");
-          }
-        );
-      } else {
-        res.writeHead(500);
-        res.end(`Server Error ${err.code}`);
-      }
-    } else {
-      res.writeHead(200, { "Content-Type": contentType });
-      res.end(content, "utf8");
-    }
-  });
+app.get("/", (req, res) => {
+    res.render("index")
 });
 
+app.get("/blog", (req, res) => {
+    res.render("blog")
+});
+
+app.get("/resume", (req, res) => {
+    res.render("resume")
+});
+
+app.get("/facts", (req, res) => {
+    res.render("facts")
+});
 
 const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
